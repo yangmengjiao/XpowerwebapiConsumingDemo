cde3133618897f7e42bdfdea808393ce65c24cc5
yangmengjiao <mengjiaoshun@gmail.com>
2020-12-22 11:34:57 -0600

    Initial Commit

---
+++ interview3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  interview3
+//
+//  Created by mengjiao on 12/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
